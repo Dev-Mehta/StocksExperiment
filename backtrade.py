c5bd4719ce1e20ea82698b262d040ba6e202549c
Dev Mehta <devbmehta04@gmail.com>
2022-05-18 15:36:03 +0530

    Added more demand zones

---
+++ backtrade.py
@@ -0,0 +1,155 @@
+import backtrader as bt
+import pandas as pd
+import os.path
+import sys
+from tabulate import tabulate
+
+class TestFeed(bt.feeds.PandasData):
+	lines = ('proximalLine', 'distalLine' ,)
+	params = (('proximalLine', 12),('distalLine', 13))
+
+class TestStrategy(bt.Strategy):
+
+	def log(self, txt, dt=None):
+		''' Logging function for this strategy'''
+		dt = dt or self.datas[0].datetime.date(0)
+		print('%s, %s' % (dt.isoformat(), txt))
+
+	def __init__(self):
+		# Keep a reference to the "close" line in the data[0] dataseries
+		self.dataclose = self.datas[0].close
+		self.order = None
+		self.buyprice = None
+		self.buycomm = None
+		self.scores = self.datas[0].proximalLine
+		self.trades = None
+		self.size = 10
+		self.stop_loss = 0.1
+		self.trailing = True
+	def notify_order(self, order):
+		if order.status in [order.Submitted, order.Accepted]:
+			# Buy/Sell order submitted/accepted to/by broker - Nothing to do
+			return
+
+		# Check if an order has been completed
+		# Attention: broker could reject order if not enough cash
+		if order.status in [order.Completed]:
+			if order.isbuy():
+				self.log(
+					'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+					(order.executed.price,
+					 order.executed.value,
+					 order.executed.comm))
+
+				self.buyprice = order.executed.price
+				self.buycomm = order.executed.comm
+			elif order.issell():
+				self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+						 (order.executed.price,
+						  order.executed.value,
+						  order.executed.comm))
+			self.bar_executed = len(self)
+
+		elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+			self.log('Order Canceled/Margin/Rejected')
+
+		# Write down: no pending order
+		self.order = None
+	def notify_trade(self, trade):
+		if not trade.isclosed:
+			return
+		self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+				 (trade.pnl, trade.pnlcomm))
+
+	def next(self):
+		# Simply log the closing price of the series from the reference
+		if self.order:
+			return
+		# Check if we are in the market
+		if not self.position:
+			# Not yet ... we MIGHT BUY if ...
+			if self.scores[0]:
+				self.log('BUY CREATE, %.2f' % self.dataclose[0])
+				# Keep track of the created order to avoid a 2nd order
+				self.order = self.buy()
+				self.sell(exectype=bt.Order.StopTrailLimit, price=self.datas[0].distalLine[0], trailpercent=0.05)
+				# self.log("SL Placed " + str(self.datas[0].distalLine[0]))
+				
+class trade_list(bt.Analyzer):
+
+	def get_analysis(self):
+
+		return self.trades
+
+
+	def __init__(self):
+
+		self.trades = []
+		self.cumprofit = 0.0
+
+
+	def notify_trade(self, trade):
+
+		if trade.isclosed:
+
+			brokervalue = self.strategy.broker.getvalue()
+
+			dir = 'short'
+			if trade.history[0].event.size > 0: dir = 'long'
+
+			pricein = trade.history[len(trade.history)-1].status.price
+			priceout = trade.history[len(trade.history)-1].event.price
+			datein = bt.num2date(trade.history[0].status.dt)
+			dateout = bt.num2date(trade.history[len(trade.history)-1].status.dt)
+			if trade.data._timeframe >= bt.TimeFrame.Days:
+				datein = datein.date()
+				dateout = dateout.date()
+
+			pcntchange = 100 * priceout / pricein - 100
+			pnl = trade.history[len(trade.history)-1].status.pnlcomm
+			pnlpcnt = 100 * pnl / brokervalue
+			barlen = trade.history[len(trade.history)-1].status.barlen
+			# pbar = pnl / barlen
+			self.cumprofit += pnl
+
+			size = value = 0.0
+			for record in trade.history:
+				if abs(size) < abs(record.status.size):
+					size = record.status.size
+					value = record.status.value
+
+			highest_in_trade = max(trade.data.high.get(ago=0, size=barlen+1))
+			lowest_in_trade = min(trade.data.low.get(ago=0, size=barlen+1))
+			hp = 100 * (highest_in_trade - pricein) / pricein
+			lp = 100 * (lowest_in_trade - pricein) / pricein
+			if dir == 'long':
+				mfe = hp
+				mae = lp
+			if dir == 'short':
+				mfe = -lp
+				mae = -hp
+			self.trades.append({'ID': trade.ref, 'Ticker': trade.data._name, 'Entry Type': dir,
+				 'Buy Date': datein, 'Buy Price': pricein, 'Sell Date': dateout, 'Sell Price': priceout,
+				 'Change %': round(pcntchange, 2), 'P&L': pnl, 'P&L%': round(pnlpcnt, 2),
+				 'Quantity': size, 'No. of days held': barlen})
+				 
+cerebro = bt.Cerebro()
+cerebro.addstrategy(TestStrategy)
+modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+datapath = os.path.join(modpath, 'infy_prep_weekly.csv')
+data = pd.read_csv(datapath)
+data.index = pd.to_datetime(data.Date)
+data = TestFeed(dataname=data)
+cerebro.adddata(data)
+cerebro.broker.setcash(100000.0)
+cerebro.broker.setcommission(commission=0.002)
+cerebro.addsizer(bt.sizers.SizerFix, stake=50)
+cerebro.addanalyzer(trade_list, _name='trade_list')
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+strats = cerebro.run(tradehistory=True)
+strat = strats[0]
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+trade_list = strats[0].analyzers.trade_list.get_analysis()
+df = pd.DataFrame(trade_list)
+df.set_index('ID', inplace=True)
+df.to_csv("backtest_results.csv")
---
+++ script.py
@@ -0,0 +1,417 @@
+# Import Libaries
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+# Read data
+# df = yf.Ticker('reliance.NS').history(period='max', interval="1wk",actions=False)
+# df.to_csv("reliance_weekly.csv")
+df = pd.read_csv("reliance.csv")
+df.reset_index()
+df.set_index('Date')
+
+# Define types of candles
+df['boringCandle'] = (abs(df['Open']-df['Close']) / abs(df['High']-df['Low']) < 0.5)
+df['greenExcitingCandle'] = (abs(df['Open']-df['Close'])/abs(df['High']-df['Low']) > 0.5) & (df['Open']<df['Close'])
+df['redExcitingCandle'] = (abs(df['Open']-df['Close'])/abs(df['High']-df['Low']) > 0.5) & (df['Open']>df['Close'])
+
+# Make patterns out of candles
+# demand zones
+"""
+Demand zone patterns
+drop base rally - base candle 1
+rally base rally - base candle 1
+drop base rally - base candle 2
+rally base rally - base candle 2 
+"""
+df['ds_dbr_base1'] = df['redExcitingCandle'].shift(2) & df['boringCandle'].shift(1) & df['greenExcitingCandle']
+df['ds_rbr_base1'] = df['greenExcitingCandle'].shift(2) & df['boringCandle'].shift(1) & df['greenExcitingCandle']
+df['ds_dbr_base2'] = df['redExcitingCandle'].shift(3) & df['boringCandle'].shift(2) & df['boringCandle'].shift(1) & df['greenExcitingCandle']
+df['ds_rbr_base2'] = df['greenExcitingCandle'].shift(3) & df['boringCandle'].shift(2) & df['boringCandle'].shift(1) & df['greenExcitingCandle']
+
+# supply zones
+"""
+Demand zone patterns
+rally base drop - base candle 1
+drop base drop - base candle 1
+rally base drop - base candle 2
+drop base drop - base candle 2 
+"""
+df['ss_rbd_base1'] = df['greenExcitingCandle'].shift(2) & df['boringCandle'].shift(1) & df['redExcitingCandle']
+df['ss_dbd_base1'] = df['redExcitingCandle'].shift(2) & df['boringCandle'].shift(1) & df['redExcitingCandle']
+df['ss_rbd_base2'] = df['greenExcitingCandle'].shift(3) & df['boringCandle'].shift(2) & df['boringCandle'].shift(1) & df['redExcitingCandle']
+df['ss_dbd_base2'] = df['redExcitingCandle'].shift(3) & df['boringCandle'].shift(2) & df['boringCandle'].shift(1) & df['redExcitingCandle']
+
+# Get base candles out of patterns - which will be later used to find proximal and distal line
+df_ds_dbr_base1 = df.iloc[df.iloc[np.where(df['ds_dbr_base1'] == True)].index - 1]
+df_ds_rbr_base1 = df.iloc[df.iloc[np.where(df['ds_rbr_base1'] == True)].index - 1]
+
+df_ss_rbd_base1 = df.iloc[df.iloc[np.where(df['ss_rbd_base1'] == True)].index - 1]
+df_ss_dbd_base1 = df.iloc[df.iloc[np.where(df['ss_dbd_base1'] == True)].index - 1]
+df_ss_rbd_base1_cmp = df.iloc[np.where(df['ss_rbd_base1'] == True)]
+df_ss_dbd_base1_cmp = df.iloc[np.where(df['ss_dbd_base1'] == True)]
+
+df_ds_dbr_base2_boringCandle1 = df.iloc[df.iloc[np.where(df['ds_dbr_base2'] == True)].index - 2]
+df_ds_dbr_base2_boringCandle2 = df.iloc[df.iloc[np.where(df['ds_dbr_base2'] == True)].index - 1]
+df_ds_rbr_base2_boringCandle1 = df.iloc[df.iloc[np.where(df['ds_rbr_base2'] == True)].index - 2]
+df_ds_rbr_base2_boringCandle2 = df.iloc[df.iloc[np.where(df['ds_rbr_base2'] == True)].index - 1]
+
+df_ss_rbd_base2_boringCandle1 = df.iloc[df.iloc[np.where(df['ss_rbd_base2'] == True)].index - 2]
+df_ss_rbd_base2_boringCandle2 = df.iloc[df.iloc[np.where(df['ss_rbd_base2'] == True)].index - 1]
+df_ss_dbd_base2_boringCandle1 = df.iloc[df.iloc[np.where(df['ss_dbd_base2'] == True)].index - 2]
+df_ss_dbd_base2_boringCandle2 = df.iloc[df.iloc[np.where(df['ss_dbd_base2'] == True)].index - 1]
+
+df_ss_rbd_base2 = df.iloc[np.where(df['ss_rbd_base2'] == True)]
+df_ss_dbd_base2 = df.iloc[np.where(df['ss_dbd_base2'] == True)]
+
+# Get color of base candle
+df_ds_dbr_base1['greenBoringCandle'] = df_ds_dbr_base1['Open'] < df_ds_dbr_base1['Close']
+df_ds_rbr_base1['greenBoringCandle'] = df_ds_rbr_base1['Open'] < df_ds_rbr_base1['Close']
+df_ds_dbr_base2_boringCandle1['greenBoringCandle'] = df_ds_dbr_base2_boringCandle1['Open'] < df_ds_dbr_base2_boringCandle1['Close']
+df_ds_dbr_base2_boringCandle2['greenBoringCandle'] = df_ds_dbr_base2_boringCandle2['Open'] < df_ds_dbr_base2_boringCandle2['Close']
+df_ds_rbr_base2_boringCandle1['greenBoringCandle'] = df_ds_rbr_base2_boringCandle1['Open'] < df_ds_rbr_base2_boringCandle1['Close']
+df_ds_rbr_base2_boringCandle2['greenBoringCandle'] = df_ds_rbr_base2_boringCandle2['Open'] < df_ds_rbr_base2_boringCandle2['Close']
+
+df_ss_rbd_base1['greenBoringCandle'] = df_ss_rbd_base1['Open'] < df_ss_rbd_base1['Close']
+df_ss_dbd_base1['greenBoringCandle'] = df_ss_dbd_base1['Open'] < df_ss_dbd_base1['Close']
+df_ss_rbd_base2_boringCandle1['greenBoringCandle'] = df_ss_rbd_base2_boringCandle1['Open'] < df_ss_rbd_base2_boringCandle1['Close']
+df_ss_rbd_base2_boringCandle2['greenBoringCandle'] = df_ss_rbd_base2_boringCandle2['Open'] < df_ss_rbd_base2_boringCandle2['Close']
+df_ss_dbd_base2_boringCandle1['greenBoringCandle'] = df_ss_dbd_base2_boringCandle1['Open'] < df_ss_dbd_base2_boringCandle1['Close']
+df_ss_dbd_base2_boringCandle2['greenBoringCandle'] = df_ss_dbd_base2_boringCandle2['Open'] < df_ss_dbd_base2_boringCandle2['Close']
+
+"""
+Find Base 1 Demand Zones - Proximal Line and Distal Line
+"""
+dbrProximalLine = []
+dbrDistalLine = []
+rbrProximalLine = []
+rbrDistalLine = []
+for i in range(0,len(df_ds_dbr_base1)):
+	df_x = df_ds_dbr_base1.iloc[i]
+	if df_x['greenBoringCandle']:
+		dbrProximalLine.append(df_x['Close'])
+	else:
+		dbrProximalLine.append(df_x['Open'])
+	dbrDistalLine.append(df_x['Low'])
+for i in range(0,len(df_ds_rbr_base1)):
+	df_x = df_ds_rbr_base1.iloc[i]
+	if df_x['greenBoringCandle']:
+		rbrProximalLine.append(df_x['Close'])
+	else:
+		rbrProximalLine.append(df_x['Open'])
+	rbrDistalLine.append(df_x['Low'])
+df_ds_dbr_base1['proximalLine'] = dbrProximalLine
+df_ds_dbr_base1['distalLine'] = dbrDistalLine
+df_ds_rbr_base1['proximalLine'] = rbrProximalLine
+df_ds_rbr_base1['distalLine'] = rbrDistalLine
+
+"""
+Finding Base 1 Supply Zones - Proximal and Distal Line
+"""
+
+rbdProximalLine = []
+rbdDistalLine = []
+dbdProximalLine = []
+dbdDistalLine = []
+for i in range(0,len(df_ss_rbd_base1)):
+	df_x = df_ss_rbd_base1.iloc[i]
+	if not df_x['greenBoringCandle']:
+		rbdProximalLine.append(df_x['Close'])
+	else:
+		rbdProximalLine.append(df_x['Open'])
+	rbdDistalLine.append(df_x['High'])
+for i in range(0,len(df_ss_dbd_base1)):
+	df_x = df_ss_dbd_base1.iloc[i]
+	if not df_x['greenBoringCandle']:
+		dbdProximalLine.append(df_x['Close'])
+	else:
+		dbdProximalLine.append(df_x['Open'])
+	dbdDistalLine.append(df_x['High'])
+df_ss_rbd_base1['proximalLine'] = rbdProximalLine
+df_ss_rbd_base1['distalLine'] = rbdDistalLine
+df_ss_dbd_base1['proximalLine'] = dbdProximalLine
+df_ss_dbd_base1['distalLine'] = dbdDistalLine
+
+"""
+Finding Base 2 Demand Zones - Proximal Line and Distal Line
+"""
+dbrProximalLine2 = []
+dbrDistalLine2 = []
+rbrProximalLine2 = []
+rbrDistalLine2 = []
+for i in range(0,len(df_ds_dbr_base2_boringCandle1)):
+	df_x_1 = df_ds_dbr_base2_boringCandle1.iloc[i]
+	df_x_2 = df_ds_dbr_base2_boringCandle2.iloc[i]
+	if df_x_1['greenBoringCandle'] and df_x_2['greenBoringCandle']:
+		if df_x_1['Close'] > df_x_2['Close']:
+			dbrProximalLine2.append(df_x_1['Close'])
+		else:
+			dbrProximalLine2.append(df_x_2['Close'])
+	elif df_x_1['greenBoringCandle'] and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] > df_x_2['Open']:
+			dbrProximalLine2.append(df_x_1['Close'])
+		else:
+			dbrProximalLine2.append(df_x_2['Open'])
+	elif (df_x_1['greenBoringCandle'] == False) and df_x_2['greenBoringCandle']:
+		if df_x_1['Open'] > df_x_2['Close']:
+			dbrProximalLine2.append(df_x_1['Open'])
+		else:
+			dbrProximalLine2.append(df_x_2['Close'])
+	else:
+		if df_x_1['Open'] > df_x_2['Open']:
+			dbrProximalLine2.append(df_x_1['Open'])
+		else:
+			dbrProximalLine2.append(df_x_2['Open'])
+	if int(df_x_1['Low']) < int(df_x_2['Low']):
+		dbrDistalLine2.append(int(df_x_1['Low']))
+	else:
+		dbrDistalLine2.append(int(df_x_2['Low']))
+for i in range(0,len(df_ds_rbr_base2_boringCandle1)):
+	df_x_1 = df_ds_rbr_base2_boringCandle1.iloc[i]
+	df_x_2 = df_ds_rbr_base2_boringCandle2.iloc[i]
+	if df_x_1['greenBoringCandle'] and df_x_2['greenBoringCandle']:
+		if df_x_1['Close'] > df_x_2['Close']:
+			rbrProximalLine2.append(df_x_1['Close'])
+		else:
+			rbrProximalLine2.append(df_x_2['Close'])
+	elif df_x_1['greenBoringCandle'] and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] > df_x_2['Open']:
+			rbrProximalLine2.append(df_x_1['Close'])
+		else:
+			rbrProximalLine2.append(df_x_2['Open'])
+	elif (df_x_1['greenBoringCandle'] == False) and df_x_2['greenBoringCandle']:
+		if df_x_1['Open'] > df_x_2['Close']:
+			rbrProximalLine2.append(df_x_1['Open'])
+		else:
+			rbrProximalLine2.append(df_x_2['Close'])
+	else:
+		if df_x_1['Open'] > df_x_2['Open']:
+			rbrProximalLine2.append(df_x_1['Open'])
+		else:
+			rbrProximalLine2.append(df_x_2['Open'])
+	if int(df_x_1['Low']) < int(df_x_2['Low']):
+		rbrDistalLine2.append(int(df_x_1['Low']))
+	else:
+		rbrDistalLine2.append(int(df_x_2['Low']))
+df_ds_dbr_base2_boringCandle1['proximalLine'] = dbrProximalLine2
+df_ds_dbr_base2_boringCandle1['distalLine'] = dbrDistalLine2
+df_ds_rbr_base2_boringCandle1['proximalLine'] = rbrProximalLine2
+df_ds_rbr_base2_boringCandle1['distalLine'] = rbrDistalLine2
+
+"""
+Finding Base 2 Supply Zones - Proximal and Distal Line
+"""
+rbdProximalLine2 = []
+rbdDistalLine2 = []
+dbdProximalLine2 = []
+dbdDistalLine2 = []
+for i in range(0,len(df_ss_rbd_base2_boringCandle1)):
+	df_x_1 = df_ss_rbd_base2_boringCandle1.iloc[i]
+	df_x_2 = df_ss_rbd_base2_boringCandle2.iloc[i]
+	if (not df_x_1['greenBoringCandle']) and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] < df_x_2['Close']:
+			rbdProximalLine2.append(df_x_1['Close'])
+		else:
+			rbdProximalLine2.append(df_x_2['Close'])
+	elif (not df_x_1['greenBoringCandle']) and (df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] < df_x_2['Open']:
+			rbdProximalLine2.append(df_x_1['Close'])
+		else:
+			rbdProximalLine2.append(df_x_2['Open'])
+	elif (df_x_1['greenBoringCandle']) and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Open'] < df_x_2['Close']:
+			rbdProximalLine2.append(df_x_1['Open'])
+		else:
+			rbdProximalLine2.append(df_x_2['Close'])
+	else:
+		if df_x_1['Open'] < df_x_2['Open']:
+			rbdProximalLine2.append(df_x_1['Open'])
+		else:
+			rbdProximalLine2.append(df_x_2['Open'])
+	if int(df_x_1['High']) > int(df_x_2['High']):
+		rbdDistalLine2.append(int(df_x_1['High']))
+	else:
+		rbdDistalLine2.append(int(df_x_2['High']))
+
+for i in range(0,len(df_ss_dbd_base2_boringCandle1)):
+	df_x_1 = df_ss_dbd_base2_boringCandle1.iloc[i]
+	df_x_2 = df_ss_dbd_base2_boringCandle2.iloc[i]
+	if (not df_x_1['greenBoringCandle']) and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] < df_x_2['Close']:
+			dbdProximalLine2.append(df_x_1['Close'])
+		else:
+			dbdProximalLine2.append(df_x_2['Close'])
+	elif (not df_x_1['greenBoringCandle']) and (df_x_2['greenBoringCandle']):
+		if df_x_1['Close'] < df_x_2['Open']:
+			dbdProximalLine2.append(df_x_1['Close'])
+		else:
+			dbdProximalLine2.append(df_x_2['Open'])
+	elif (df_x_1['greenBoringCandle']) and (not df_x_2['greenBoringCandle']):
+		if df_x_1['Open'] < df_x_2['Close']:
+			dbdProximalLine2.append(df_x_1['Open'])
+		else:
+			dbdProximalLine2.append(df_x_2['Close'])
+	else:
+		if df_x_1['Open'] < df_x_2['Open']:
+			dbdProximalLine2.append(df_x_1['Open'])
+		else:
+			dbdProximalLine2.append(df_x_2['Open'])
+	if int(df_x_1['High']) > int(df_x_2['High']):
+		dbdDistalLine2.append(int(df_x_1['High']))
+	else:
+		dbdDistalLine2.append(int(df_x_2['High']))
+df_ss_rbd_base2_boringCandle1['proximalLine'] = rbdProximalLine2
+df_ss_rbd_base2_boringCandle1['distalLine'] = rbdDistalLine2
+df_ss_dbd_base2_boringCandle1['proximalLine'] = dbdProximalLine2
+df_ss_dbd_base2_boringCandle1['distalLine'] = dbdDistalLine2
+
+"""Removing Tested Demand Zones"""
+df_ds_dbr_base1_list = df_ds_dbr_base1.values.tolist()
+df_ds_rbr_base1_list = df_ds_rbr_base1.values.tolist()
+df_ds_dbr_base2_boringCandle1_list = df_ds_dbr_base2_boringCandle1.values.tolist()
+df_ds_rbr_base2_boringCandle1_list = df_ds_rbr_base2_boringCandle1.values.tolist()
+df_ds_dbr_base1_list = df_ds_dbr_base1_list[::-1]
+df_ds_rbr_base1_list = df_ds_rbr_base1_list[::-1]
+df_ds_dbr_base2_boringCandle1_list = df_ds_dbr_base2_boringCandle1_list[::-1]
+df_ds_rbr_base2_boringCandle1_list = df_ds_rbr_base2_boringCandle1_list[::-1]
+zoneTested = []
+df.Date = pd.to_datetime(df.Date)
+linesToDraw = []
+for dz in df_ds_dbr_base1_list:
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = dz[0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.Low <= proximalLine)) == 0:
+		linesToDraw.append([index,proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ds_dbr_base1['zoneTested'] = zoneTested
+zoneTested = []	
+for dz in df_ds_rbr_base1_list:
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = dz[0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.Low <= proximalLine)[0]) == 0:
+		linesToDraw.append([index,proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ds_rbr_base1['zoneTested'] = zoneTested
+zoneTested = []	
+for i in range(0,len(df_ds_dbr_base2_boringCandle1_list)):
+	dz = df_ds_dbr_base2_boringCandle1.iloc[i]
+	dz2 = df_ds_dbr_base2_boringCandle2.iloc[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = dz2[0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.Low <= proximalLine)[0]) == 0:
+		linesToDraw.append([index,proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ds_dbr_base2_boringCandle1['zoneTested'] = zoneTested
+zoneTested = []	
+for i in range(0,len(df_ds_rbr_base2_boringCandle1_list)):
+	dz = df_ds_rbr_base2_boringCandle1.iloc[i]
+	dz2 = df_ds_rbr_base2_boringCandle2.iloc[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = dz2[0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.Low <= proximalLine)[0]) == 0:
+		linesToDraw.append([index,proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ds_rbr_base2_boringCandle1['zoneTested'] = zoneTested
+
+"""Removing Tested Supply Zones"""
+df_ss_rbd_base1_list = df_ss_rbd_base1.values.tolist()
+df_ss_dbd_base1_list = df_ss_dbd_base1.values.tolist()
+df_ss_rbd_base1_list_cmp = df_ss_rbd_base1_cmp.values.tolist()
+df_ss_dbd_base1_list_cmp = df_ss_dbd_base1_cmp.values.tolist()
+df_ss_rbd_base2_boringCandle1_list = df_ss_rbd_base2_boringCandle1.values.tolist()
+df_ss_dbd_base2_boringCandle1_list = df_ss_dbd_base2_boringCandle1.values.tolist()
+df_ss_rbd_base1_list = df_ss_rbd_base1_list[::-1]
+df_ss_dbd_base1_list = df_ss_dbd_base1_list[::-1]
+df_ss_rbd_base1_list_cmp = df_ss_rbd_base1_list_cmp[::-1]
+df_ss_dbd_base1_list_cmp = df_ss_dbd_base1_list_cmp[::-1]
+df_ss_rbd_base2_boringCandle1_list = df_ss_rbd_base2_boringCandle1_list[::-1]
+df_ss_dbd_base2_boringCandle1_list = df_ss_dbd_base2_boringCandle1_list[::-1]
+zoneTested = []
+df.Date = pd.to_datetime(df.Date)
+linesToDrawSZ = []
+for i in range(0,len(df_ss_rbd_base1_list)):
+	dz = df_ss_rbd_base1_list[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = df_ss_rbd_base1_list_cmp[i][0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.High >= proximalLine)[0]) == 0:
+		print("Zone found")
+		linesToDrawSZ.append([dz[0],proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ss_rbd_base1['zoneTested'] = zoneTested
+zoneTested = []	
+for i in range(0,len(df_ss_dbd_base1_list)):
+	dz = df_ss_dbd_base1_list[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = df_ss_dbd_base1_list_cmp[i][0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.High >= proximalLine)[0]) == 0:
+		linesToDrawSZ.append([dz[0],proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ss_dbd_base1['zoneTested'] = zoneTested
+zoneTested = []	
+for i in range(0,len(df_ss_rbd_base2_boringCandle1_list)):
+	dz = df_ss_rbd_base2_boringCandle1.iloc[i]
+	dz2 = df_ss_rbd_base2_boringCandle2.iloc[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = df_ss_rbd_base2.iloc[i][0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.High >= proximalLine)[0]) == 0:
+		linesToDrawSZ.append([dz2[0],proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ss_rbd_base2_boringCandle1['zoneTested'] = zoneTested
+zoneTested = []	
+for i in range(0,len(df_ss_dbd_base2_boringCandle1_list)):
+	dz = df_ss_dbd_base2_boringCandle1.iloc[i]
+	dz2 = df_ss_dbd_base2_boringCandle2.iloc[i]
+	proximalLine = dz[18]
+	distalLine = dz[19]
+	index = df_ss_dbd_base2.iloc[i][0]
+	index = pd.to_datetime(index)
+	dz_sub = df.loc[df.Date > index]
+	if len(np.where(dz_sub.High >= proximalLine)[0]) == 0:
+		linesToDrawSZ.append([dz2[0],proximalLine, distalLine])
+		zoneTested.append(False)
+	else:
+		zoneTested.append(True)
+df_ss_dbd_base2_boringCandle1['zoneTested'] = zoneTested
+fig = go.Figure(data=go.Candlestick(x=df['Date'], open=df['Open'], close=df['Close'], low=df['Low'], high=df['High']))
+for row in linesToDrawSZ:
+	fig.add_shape(type="rect",line=dict(color="red", width=2),x0=row[0], y0=row[2], x1=df.Date.max(), y1=row[1], fillcolor='red', opacity=0.5)
+for row in linesToDraw:
+	fig.add_shape(type="rect",line=dict(color="green", width=2),x0=row[0], y0=row[1], x1=df.Date.max(), y1=row[2], fillcolor='green', opacity=0.5)
+fig.update_layout(margin={"t":25, "b":0, "l":0, "r":2})
+fig.write_html("output/reliance_monthly.html")
